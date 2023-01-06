2b3808ce614d6e82d020fb11d7b33563e25c0c4a
skwazz <skwazz@gmail.com>
2023-01-06 11:49:32 +0200

    api change

---
+++ client/build/precache-manifest.3ab48a0e19b72f9ed9e633b6a27da90c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "143256ee8671818c55f04a33dce3c658",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b83b48085238da94fc4",
+    "url": "/static/css/main.9516a8fc.chunk.css"
+  },
+  {
+    "revision": "94722200f2bb2c54f87b",
+    "url": "/static/js/2.32d91e32.chunk.js"
+  },
+  {
+    "revision": "c06d497d6716e84111c2fd9253bcfc02",
+    "url": "/static/js/2.32d91e32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b83b48085238da94fc4",
+    "url": "/static/js/main.c9b3d40d.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "dc8bbfe30b9b1cf8867a83e69d051651",
+    "url": "/static/media/memoriesLogo.dc8bbfe3.png"
+  },
+  {
+    "revision": "2b89c5fae2d163d5a126a519646883fa",
+    "url": "/static/media/memoriesText.2b89c5fa.png"
+  }
+]);
