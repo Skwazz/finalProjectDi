98e401801bc7d73ccf974928dce15394517e23c9
skwazz <skwazz@gmail.com>
2023-01-06 12:17:33 +0200

    change

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://memori3s.netlify.app' });
+const API = axios.create({ baseURL: '//memori3s.netlify.app' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
